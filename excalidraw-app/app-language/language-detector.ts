11efb2a04b943f311ab77f2bf47bf00fabd3e714
git80123 <linqinghai80@gmail.com>
2025-04-21 23:16:05 +0800

    Update language-detector.ts

--- excalidraw-app/app-language/language-detector.ts
+++ excalidraw-app/app-language/language-detector.ts
@@ -2,24 +2,21 @@ import { defaultLang, languages } from "@excalidraw/excalidraw";
 import LanguageDetector from "i18next-browser-languagedetector";
 
 export const languageDetector = new LanguageDetector();
-
 languageDetector.init({
   languageUtils: {},
 });
 
 export const getPreferredLanguage = () => {
   const detectedLanguages = languageDetector.detect();
-
   const detectedLanguage = Array.isArray(detectedLanguages)
     ? detectedLanguages[0]
     : detectedLanguages;
 
-  const initialLanguage =
-    (detectedLanguage
-      ? // region code may not be defined if user uses generic preferred language
-        // (e.g. chinese vs instead of chinese-simplified)
-        languages.find((lang) => lang.code.startsWith(detectedLanguage))?.code
-      : null) || defaultLang.code;
+  // 检测到的语言
+  const detectedLang = detectedLanguage
+    ? languages.find((lang) => lang.code.startsWith(detectedLanguage))
+    : null;
 
-  return initialLanguage;
+  // 如果检测到的语言存在，使用它；否则使用中文
+  return detectedLang ? detectedLang.code : 'zh';
 };
